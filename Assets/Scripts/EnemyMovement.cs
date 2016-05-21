fe7f6846dbb3e9c6b43c6a71ecf859640b010668
Andre Rattinger <andre.rattinger@gmail.com>
2016-05-21 16:22:42 +0200

    Rearanged project structure

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour {
+
+    Transform target;
+    NavMeshAgent nav;
+
+	// Use this for initialization
+    void Awake()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        nav = GetComponent<NavMeshAgent>();
+
+    }
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        nav.SetDestination(target.position);
+	}
+}
